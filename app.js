b21ac8b9370cf6eb46162acaf7c1baa2b0a52dc3
sipsdaoracle <siphokazidolo@gmail.com>
2023-06-01 01:46:14 +0200

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Siphokazi Dolo!'));
+app.get('/', (req, res) => res.send('Siphokazi Dolo Test!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
